e7df6b690105694950cc35c27f5c949557a91db4
Shane Volpe <shanevolpe@gmail.com>
2025-09-02 14:17:07 -0400

    Added functionality of editing existing torrent requests: - Added query to return a torrent request based on ID - Added query to update/edit an existing query - Updated torrent request routes to include a put
    Closes #313

---
+++ backend/api/src/handlers/torrent_requests/edit_torrent_request.rs
@@ -0,0 +1,42 @@
+use actix_web::{
+    web::{Data, Json},
+    HttpResponse,
+};
+use arcadia_storage::{
+    models::torrent_request::{EditedTorrentRequest, TorrentRequest},
+    redis::RedisPoolInterface,
+};
+
+use crate::{middlewares::jwt_middleware::Authdata, Arcadia};
+use arcadia_common::error::{Error, Result};
+
+#[utoipa::path(
+    put,
+    operation_id = "Edit torrent request",
+    tag = "Torrent Request",
+    path = "/api/torrent-requests",
+    security(
+        ("http" = ["Bearer"])
+    ),
+    responses(
+        (status = 200, description = "Successfully edited the torrent request", body=TorrentRequest),
+    )
+)]
+
+pub async fn exec<R: RedisPoolInterface + 'static>(
+    form: Json<EditedTorrentRequest>,
+    arc: Data<Arcadia<R>>,
+    user: Authdata,
+) -> Result<HttpResponse> {
+    let torrent_request = arc.pool.find_torrent_request(form.id).await?;
+
+    if torrent_request.created_by_id == user.sub || user.class == "staff" {
+        let updated_torrent_request = arc
+            .pool
+            .update_torrent_request(&form, torrent_request.id)
+            .await?;
+        Ok(HttpResponse::Ok().json(updated_torrent_request))
+    } else {
+        Err(Error::InsufficientPrivileges)
+    }
+}
