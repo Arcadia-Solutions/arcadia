c3c1a9cace35e3f9a4984bab60c40923006386b2
NathanJ60 <jacas.nathan@gmail.com>
2025-12-09 22:35:18 +0100

    feat: forum search bar redirects to search page on enter
    Fixes #422

--- frontend/src/components/SearchBars.vue
+++ frontend/src/components/SearchBars.vue
@@ -5,16 +5,17 @@
       :placeholder="t('torrent.torrent', 2)"
       v-model="searchForm.torrents"
       size="small"
-      @keyup.enter="
-        () => {
-          router.push(`/torrents?title_group_name=${searchForm.torrents}`)
-          searchForm.torrents = ''
-        }
-      "
+      @keyup.enter="searchTorrents"
     />
     <ArtistSearchBar :placeholder="t('artist.artist', 2)" :clickableSeriesLink="true" :clearInputOnSelect="true" v-model="searchForm.artists" />
     <SeriesSearchBar :placeholder="t('series.series')" :clickableSeriesLink="true" :clearInputOnSelect="true" v-model="searchForm.series" />
-    <InputText type="text" :placeholder="t('forum.forum', 2)" v-model="searchForm.forums" size="small" />
+    <InputText
+      type="text"
+      :placeholder="t('forum.forum', 2)"
+      v-model="searchForm.forums"
+      size="small"
+      @keyup.enter="searchForums"
+    />
     <InputText type="text" :placeholder="t('user.user', 2)" v-model="searchForm.users" size="small" />
   </div>
 </template>
@@ -38,6 +39,22 @@ const searchForm = ref({
   forums: '',
   users: '',
 })
+
+const searchTorrents = () => {
+  router.push({
+    path: '/torrents',
+    query: { title_group_name: searchForm.value.torrents }
+  })
+  searchForm.value.torrents = ''
+}
+
+const searchForums = () => {
+  router.push({
+    path: '/forum/search',
+    query: { thread_name: searchForm.value.forums }
+  })
+  searchForm.value.forums = ''
+}
 </script>
 
 <style scoped>
