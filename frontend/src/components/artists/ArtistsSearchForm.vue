c6f830add576028f9f0b15c39d41cdd40ab6ae98
FrenchGithubUser <frenchgithubuser@tutanota.com>
2026-01-01 21:12:21 +0100

    fix: display pagination on artist search page

--- frontend/src/components/artists/ArtistsSearchForm.vue
+++ frontend/src/components/artists/ArtistsSearchForm.vue
@@ -1,8 +1,8 @@
 <template>
   <ContentContainer>
-    <Form @submit="fetchArtists">
+    <Form @submit="search">
       <FloatLabel>
-        <InputText v-model="form.name" name="name" size="small" />
+        <InputText v-model="searchForm.name" name="name" size="small" />
         <label for="name">{{ t('general.name') }}</label>
       </FloatLabel>
       <div class="wrapper-center" style="margin-top: 15px">
@@ -16,30 +16,58 @@
 import ContentContainer from '../ContentContainer.vue'
 import { InputText, Button, FloatLabel } from 'primevue'
 import { Form } from '@primevue/forms'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { searchArtists, type SearchArtistsRequest, type PaginatedResultsArtistSearchResult } from '@/services/api-schema'
+import { useRouter } from 'vue-router'
+import type { SearchArtistsRequest } from '@/services/api-schema'
 
 const { t } = useI18n()
+const router = useRouter()
 
-const form = ref<SearchArtistsRequest>({
+const props = defineProps<{
+  loading: boolean
+  initialForm: SearchArtistsRequest
+}>()
+
+const searchForm = ref<SearchArtistsRequest>({
   name: '',
   page: 1,
   page_size: 50,
 })
-const loading = ref(false)
 
-onMounted(async () => {
-  await fetchArtists()
-})
-
-const fetchArtists = async () => {
-  loading.value = true
-  const response = await searchArtists(form.value).finally(() => (loading.value = false))
-  emit('gotResults', response)
+const changePage = (page: number) => {
+  searchForm.value.page = page
+  search()
 }
 
-const emit = defineEmits<{
-  gotResults: [PaginatedResultsArtistSearchResult]
-}>()
+const search = () => {
+  router.push({
+    query: Object.fromEntries(
+      Object.entries({
+        name: searchForm.value.name,
+        page: searchForm.value.page,
+        page_size: searchForm.value.page_size,
+      }).filter(([, v]) => v !== undefined && v !== null && v !== ''),
+    ),
+  })
+}
+
+defineExpose({
+  searchForm,
+  changePage,
+})
+
+onMounted(async () => {
+  searchForm.value = props.initialForm
+})
+
+watch(
+  () => searchForm.value,
+  (newVal, oldVal) => {
+    if (newVal.page === oldVal.page) {
+      searchForm.value.page = 1
+    }
+  },
+  { deep: true },
+)
 </script>
--- frontend/src/views/artists/ArtistsSearchView.vue
+++ frontend/src/views/artists/ArtistsSearchView.vue
@@ -1,24 +1,79 @@
 <template>
-  <div class="wrapper-center actions">
-    <i class="pi pi-bookmark" v-tooltip.top="t('artist.bookmarked_artists')" />
+  <div v-if="initialForm">
+    <div class="wrapper-center actions">
+      <i class="pi pi-bookmark" v-tooltip.top="t('artist.bookmarked_artists')" />
+    </div>
+    <ArtistsSearchForm ref="searchFormRef" :initialForm :loading style="margin-bottom: 15px" />
+    <PaginatedResults
+      v-if="initialForm"
+      :totalPages
+      :initialPage="initialForm.page"
+      :totalItems="totalResults"
+      :pageSize
+      @changePage="searchFormRef.changePage($event.page)"
+    >
+      <ArtistsTable :artists />
+    </PaginatedResults>
   </div>
-  <ArtistsSearchForm @gotResults="gotResults" style="margin-bottom: 15px" />
-  <ArtistsTable :artists />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed, onMounted, watch, nextTick } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { useRoute } from 'vue-router'
+import type { VNodeRef } from 'vue'
 import ArtistsSearchForm from '@/components/artists/ArtistsSearchForm.vue'
 import ArtistsTable from '@/components/artists/ArtistsTable.vue'
-import type { PaginatedResultsArtistSearchResult, ArtistSearchResult } from '@/services/api-schema'
+import PaginatedResults from '@/components/PaginatedResults.vue'
+import { searchArtists, type SearchArtistsRequest, type ArtistSearchResult } from '@/services/api-schema'
 
 const { t } = useI18n()
-const artists = ref<ArtistSearchResult[]>([])
+const route = useRoute()
 
-const gotResults = (response: PaginatedResultsArtistSearchResult) => {
-  artists.value = response.results
+const searchFormRef = ref<VNodeRef | null>(null)
+
+const artists = ref<ArtistSearchResult[]>([])
+const loading = ref(false)
+const initialForm = ref<SearchArtistsRequest | null>(null)
+const totalResults = ref(0)
+const pageSize = ref(0)
+const totalPages = computed(() => Math.ceil(totalResults.value / pageSize.value))
+
+const search = async (form: SearchArtistsRequest) => {
+  loading.value = true
+  const results = await searchArtists(form).finally(() => {
+    loading.value = false
+  })
+  pageSize.value = form.page_size
+  totalResults.value = results.total_items
+  artists.value = results.results
 }
+
+const loadFormFromUrl = async () => {
+  loading.value = true
+  initialForm.value = null
+  await nextTick()
+  const form: SearchArtistsRequest = {
+    name: route.query.name?.toString() ?? '',
+    page: route.query.page ? parseInt(route.query.page as string) : 1,
+    page_size: route.query.page_size ? parseInt(route.query.page_size as string) : 50,
+  }
+  initialForm.value = form
+  pageSize.value = initialForm.value.page_size
+  search(initialForm.value)
+}
+
+onMounted(async () => {
+  loadFormFromUrl()
+})
+
+watch(
+  () => route.query,
+  () => {
+    loadFormFromUrl()
+  },
+  { deep: true },
+)
 </script>
 
 <style scoped>
