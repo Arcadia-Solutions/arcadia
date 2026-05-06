eba613b821099df630bd1979f9863eec5c0bdc33
Andres Boulanger <andresboulanger94@gmail.com>
2026-05-07 03:26:11 +1200

    Tests

---
+++ backend/api/tests/fixtures/with_test_forum_reaction.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+  forum_post_reactions (id, forum_post_id, user_id, emoji, created_at, updated_at)
+VALUES
+  (100, 100, 100, '🥰', '2025-09-17 12:42:13.702455+00', '2025-09-17 12:42:13.702455+00');
